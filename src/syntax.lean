96c3bfe74c62f050c903381c9a2dec213fb8b2ad
Hans-Dieter Hiep <hdh@cwi.nl>
2019-02-06 13:17:28 +0100

    Global names, from which program signatures are defined

--- src/syntax.lean
+++ src/syntax.lean
@@ -1,30 +1,34 @@
 -- (C) Copyright 2019, Hans-Dieter Hiep
 
-import data.finmap
+import data.finmap data.fintype
 
 section syntax
 
+variable (α : Type)
+
+-- Names have decidable equality.
+class names := (deceq: decidable_eq α)
+
 /-
-We have the following class of names.
-Names have decidable equality.
+We have the following class of dynamic names.
 Given a finite set of names,
 there can always be given a fresh instance not in that set.
 (with thanks to Johannes Hölzl)
 -/
-class fixed_names (α : Type) :=
-(deceq: decidable_eq α)
-class names (α : Type) extends fixed_names α :=
+class dynamic_names extends names α :=
 (fresh: finset α → α)
 (fresh_is_new: ∀x : finset α, (fresh x) ∉ x)
 
--- Class names (Nc)
-variables {Nc : Type} [HNc: fixed_names Nc]
--- Field names (Nf)
-variables {Nf : Type} [HNf: fixed_names Nf]
--- Method names (Nm)
-variables {Nm : Type} [HNm: fixed_names Nm]
--- Local variables (V)
-variables {V : Type}  [HV: names V]
+/-
+Each program has global names.
+Global names consist of a finite set of class names.
+To each class name, there an associated finite set of field and method names.
+-/
+class global_names (α : Type) extends names α :=
+(Nc: finset α)
+(Nf (x : α) (H: x ∈ Nc): finset α)
+(Nm (x : α) (H: x ∈ Nc): finset α)
+open global_names
 
 /-
 A type in our object language is either:
@@ -32,38 +36,34 @@ a reference type (by class name),
 a future of some type,
 or a data type from the host language.
 -/
-inductive type : Type 1
-| reference: Nc -> type
+inductive type [global_names α] : Type 1
+| reference (x : α) (H: x ∈ Nc α): type
 | future: type -> type
 | data: Type -> type
 
-structure vardecl : Type 1 :=
-(ty : @type Nc) (var : V)
-
 /-
 A program signature consists of:
-a finite map from class names to class declarations, and
-a (declared) class name of the root object.
+a map from class names to class declarations, and
+a class name of the root object.
 
-A class declaration consists of:
-a finite map from field names to types, and
-a finite map from method names to method declarations.
+A class declaration (of some class self) consists of:
+a map from self's field names to types, and
+a map from self's method names to method declarations.
 
 A method declaration consists of:
-a return type and a list of typed formal parameters.
+a return type and a list of types of its parameters.
 -/
 
-structure mdecl : Type 1 :=
-(rtype : @type Nc)
-(fparam : list (@vardecl Nc V))
+structure mdecl [global_names α] : Type 1 :=
+(retype : type α)
+(fparam : list (type α))
 
-structure cdecl : Type 1 :=
-(Mf : finmap Nf (λ_, @type Nc))
-(Mm : finmap Nm (λ_, @mdecl Nc V))
+structure cdecl [global_names α] (self : α) (H: self ∈ Nc α) : Type 1 :=
+(Mf (x : α) (H: x ∈ Nf self H) : type α)
+(Mm (x : α) (H: x ∈ Nm self H) : mdecl α)
 
-structure signature : Type 1 :=
-(Mc : finmap Nc (λ_, @cdecl Nc Nf Nm V))
-(main : Nc)
-(main_is_decl : main ∈ Mc)
+structure signature [global_names α] : Type 1 :=
+(Mc (x : α) (H: x ∈ Nc α): cdecl α x H)
+(main (x : α): x ∈ Nc α)
 
 end syntax
