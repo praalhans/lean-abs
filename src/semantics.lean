8ff63361dc905bb13cb9cf8b526a0636477600f3
Hans-Dieter Hiep <hdh@cwi.nl>
2019-02-08 15:13:34 +0100

    Source clean up and formatting, prepare for print

--- src/semantics.lean
+++ src/semantics.lean
@@ -4,150 +4,113 @@ import syntax data.stream
 
 open stream signature type pexp nat
 
-/-
-We have the following class of dynamic types.
-Given a finite set,
-there can always be given a fresh instance not in that set.
-(with thanks to Johannes Hölzl)
--/
+/- We have the following class of dynamic types. Given a finite set, there can always be given a fresh instance not in that set. (Thanks to Johannes Hölzl!) -/
 class dynamic (β : Type) :=
 (fresh: finset β → β)
 (fresh_is_new: ∀x : finset β, (fresh x) ∉ x)
 open dynamic
 
-/-
-Fix a signature (and global names).
-We treat objects transparently.
-Each object is associated to a single class name.
-Equality is decidable for objects of the same class.
-Given a set of objects, we can always construct some new object.
--/
+/- Fix a signature (and global names). We treat objects transparently. Each object is associated to a single class name. Equality is decidable for objects of the same class. Given a set of objects, we can always construct some new object. -/
 class objects (α β : Type) extends signature α, dynamic β :=
 (class_of: β → class_name α)
 (equality (x y : β): class_of x = class_of y → bool)
 open objects
 
-/-
-We treat values as being of a type.
-A value of a reference type is an object of the same class.
-A value of a data type is a term of the type in the host language.
--/
-inductive value {α : Type} (β : Type) [objects α β] : type α → Type 1
+/- We treat values as being of a type. A value of a reference type is an object of the same class. A value of a data type is a term of the type in the host language. -/
+inductive value {α : Type} (β : Type) [objects α β] :
+  type α → Type 1
 | object (o : β) : value (ref (class_of α o))
 | term {γ : Type} : γ → value (data α γ)
 
 -- Projection of value to object
-def value.objective {α β : Type} [objects α β] {c : class_name α}
-(x : value β (ref c)) : β :=
+def value.objective {α β : Type} [objects α β]
+  {c : class_name α} (x : value β (ref c)) : β :=
 begin cases x, apply x_1 end
 
-lemma value.obj_class {α β : Type} [objects α β] {c : class_name α}
-(x : value β (ref c)) : class_of α (value.objective x) = c :=
+lemma value.obj_class {α β : Type} [objects α β]
+  {c : class_name α} (x : value β (ref c)) :
+  class_of α (value.objective x) = c :=
 begin cases x, unfold value.objective end
 
 -- Projection of value to term
-def value.terminal {α : Type} (β : Type) [objects α β] {γ : Type}
-(x : value β (data α γ)) : γ :=
+def value.terminal {α : Type} (β : Type) [objects α β]
+  {γ : Type} (x : value β (data α γ)) : γ :=
 begin cases x, apply x_a end
 
-/-
-Given two values of reference type,
-we can decide referential equality.
--/
-def value.ref_equal {α β : Type} [objects α β] {c : class_name α}
-(x : value β (ref c)) (y : value β (ref c)) : bool :=
+/- Given two values of reference type, we can decide referential equality. -/
+def value.ref_equal {α β : Type} [objects α β]
+  {c : class_name α} (x : value β (ref c))
+  (y : value β (ref c)) : bool :=
 let H : class_of α (value.objective x) =
-    class_of α (value.objective y) := begin
-    repeat {rewrite value.obj_class}
-end in equality (value.objective x) (value.objective y) H
+    class_of α (value.objective y) :=
+  begin repeat {rewrite value.obj_class} end
+in equality (value.objective x) (value.objective y) H
 
 open value
 
-/-
-For class C we have a state space Σ(C) consisting of:
-an assignment of fields to values.
--/
+/- For class C we have a state space Σ(C) consisting of: an assignment of fields to values. -/
 structure state_space {α : Type} (β : Type) [objects α β]
-(self : class_name α) : Type 1 :=
+  (self : class_name α) : Type 1 :=
 (map (f : field_name self) : value β (field_type f))
 
-/-
-Given a list of types,
-we have a value list of values with matching types.
--/
-inductive vallist {α : Type} (β : Type) [objects α β] : list (type α) → Type 1
+/- Given a list of types, we have a value list of values with matching types. -/
+inductive vallist {α : Type} (β : Type) [objects α β] :
+  list (type α) → Type 1
 | nil : vallist []
 | cons {x : type α} {xs : list (type α)} :
-    value β x → vallist xs → vallist (x::xs)
+  value β x → vallist xs → vallist (x::xs)
 
-/-
-Given a value list, and an index of its type list, we can obtain a value.
--/
+/- Given a value list, and an index of its type list, we can obtain a value. -/
 def vallist.lookup {α β : Type} [objects α β] :
-Π {l : context α} {ty : type α}, vallist β l → list_at ty l → value β ty
+  Π {l : context α} {ty : type α}, vallist β l → list_at ty l →
+  value β ty
 | (x :: xs) ._ (vallist.cons (v : value β x) _)
-    (list_at.head .(x) .(xs)) := v
+  (list_at.head .(x) .(xs)) := v
 | (x :: xs) ty (vallist.cons _ (ys : vallist β xs))
-    (list_at.tail .(x) (zs : list_at .(ty) xs)) := vallist.lookup ys zs
+  (list_at.tail .(x) (zs : list_at .(ty) xs)) :=
+    vallist.lookup ys zs
 
-/-
-Given a signature and method name m,
-we have a argument space Σ(m) consisting
-an assignment of method parameters to values.
--/
+/- Given a signature and method name m, we have a argument space Σ(m) consisting an assignment of method parameters to values. -/
 structure arg_space {α : Type} (β : Type) [objects α β]
-{self : class_name α} (m : method_name self) : Type 1 :=
+  {self : class_name α} (m : method_name self) : Type 1 :=
 (map : vallist β (method_params m))
 
-/-
-Given a type environment,
-we consider an assignment to consist of:
-a state space (of the self class),
-an argument space (of the current method),
-a value list (of the local variables)
--/
-structure assignment {α : Type} (β : Type) [objects α β] (e : tenv α) :=
+/- Given a type environment, we consider an assignment to consist of: a state space (of the self class), an argument space (of the current method), a value list (of the local variables) -/
+structure assignment {α : Type} (β : Type) [objects α β]
+  (e : tenv α) :=
 (state : state_space β e.self)
 (args : arg_space β e.current)
 (store : vallist β e.locals)
 
 def assignment.lookup {α β : Type} [objects α β] {e : tenv α}
-(σ : assignment β e) (tx : type α) : rvar e tx → value β tx
+  (σ : assignment β e) (tx : type α) : rvar e tx → value β tx
 | (rvar.fvar f) := eq.mpr (congr_arg _ f.H) (σ.state.map f.idx)
 | (rvar.pvar p) := σ.args.map.lookup p.idx
 | (rvar.lvar l) := σ.store.lookup l.idx
 
-/-
-NOTE: the definition for (rvar.fvar f) was found by the following proof,
+/- NOTE: the definition for (rvar.fvar f) was found by the following proof,
 for matching the type of tx to that of the field.
 
 begin
     have H : (tx = field_type f.idx), apply f.H,
     rewrite H,
     exact σ.state.map f.idx
-end
--/
+end -/
 
-/-
-Evaluating a pure expression in an assignment.
--/
-def peval {α β : Type} [objects α β] {e : tenv α} (σ : assignment β e)
-: Π {ty : type α}, pexp e ty → value β ty
+/- Evaluating a pure expression in an assignment. -/
+def peval {α β : Type} [objects α β] {e : tenv α}
+  (σ : assignment β e) : Π {ty : type α}, pexp e ty → value β ty
 | bool (requal (c : class_name α)
-  (l : pexp e (ref c)) (r : pexp e (ref c))) :=
-    term β (ref_equal (peval l) (peval r))
+    (l : pexp e (ref c)) (r : pexp e (ref c))) :=
+  term β (ref_equal (peval l) (peval r))
 | ty (lookup (r : rvar e ty)) := σ.lookup ty r
 | _ (value _ (γ : Type) (v : γ)) := term β v
-| _ (apply (γ : Type) δ
-  (pl : pexp e (γ → δ))
-  (pr : pexp e γ)) :=
-    term β ((terminal β (peval pl)) (terminal β (peval pr)))
+| _ (apply (γ : Type) δ (pl : pexp e (γ → δ)) (pr : pexp e γ)) :=
+  term β ((terminal β (peval pl)) (terminal β (peval pr)))
 
-/-
-Evaluating a statement in an assignment
-generates a stream of assignments.
--/
-def eval {α β : Type} [objects α β] {e : tenv α} (σ : assignment β e)
-: stmt e → stream (option (assignment β e))
+/- Evaluating a statement in an assignment generates a stream of assignments. -/
+def eval {α β : Type} [objects α β] {e : tenv α}
+  (σ : assignment β e) : stmt e →
+  stream (option (assignment β e))
 | (stmt.skip e) := cons σ (const none)
-| (stmt.seq l r) := sorry
+| _ := sorry
--- src/syntax.lean
+++ src/syntax.lean
@@ -6,11 +6,7 @@ import util
 -- Names have decidable equality.
 class names (α : Type) := [deceq: decidable_eq α]
 
-/-
-Each program has global names.
-Global names consist of a finite set of class names.
-To each class name, there an associated finite set of field and method names.
--/
+/- Each program has global names. Global names consist of a finite set of class names. To each class name, there an associated finite set of field and method names. -/
 class global_names (α : Type) extends names α :=
 (Nc: finset α)
 (Nf (x : α) (H: x ∈ Nc): finset α)
@@ -19,168 +15,112 @@ class global_names (α : Type) extends names α :=
 structure class_name (α : Type) [global_names α] :=
 (name : α) (H : name ∈ global_names.Nc α)
 
-structure field_name {α : Type} [global_names α] (self : class_name α) :=
+structure field_name {α : Type} [global_names α]
+  (self : class_name α) :=
 (name : α) (H: name ∈ global_names.Nf self.name self.H)
 
-structure method_name {α : Type} [global_names α] (self : class_name α) :=
+structure method_name {α : Type} [global_names α]
+  (self : class_name α) :=
 (name : α) (H: name ∈ global_names.Nm self.name self.H)
 
-/-
-A type in our object language is either:
-a reference type (by class name),
-a future of some type,
-or a data type from the host language.
--/
+/- A type in our object language is either: a reference type (by class name), a future of some type, or a data type from the host language. -/
 inductive type (α : Type) [global_names α] : Type 1
 | ref: class_name α → type
---| future: type → type
 | data: Type → type
 
-instance Type_to_type (α : Type) [global_names α] : has_coe Type (type α) :=
-    ⟨type.data α⟩
+instance Type_to_type (α : Type) [global_names α]
+  : has_coe Type (type α) :=
+⟨type.data α⟩
 
-def type.void (α : Type) [global_names α] : type α := type.data α unit
+def type.void (α : Type) [global_names α] : type α :=
+type.data α unit
 
 open type
 
-/-
-A variable context is a list of types;
-we employ De Bruijn encoding for indexing in this context.
--/
+/- A variable context is a list of types; a variable is encoded by its index in this context. -/
 @[reducible]
 def context (α : Type) [global_names α] : Type 1 := list (type α)
 
-/-
-A program signature consists of:
-a map from class names to class declarations, and
-a class name of the root object.
-
-A class declaration (of some class self) consists of:
-a map from self's field names to types, and
-a map from self's method names to method declarations.
-
-A method declaration consists of:
-a return type and a list of types of its parameters.
--/
-
+/- A program signature consists of: a map from class names to class declarations, and a class name of the root object. A class declaration (of some class self) consists of: a map from self's field names to types, and a map from self's method names to method declarations. A method declaration consists of: a return type and a list of types of its parameters. -/
 structure mdecl {α : Type} [global_names α]
-{self : class_name α} (m : method_name self) : Type 1 :=
-(retype : type α)
-(fparam : context α)
+  {self : class_name α} (m : method_name self) : Type 1 :=
+(retype : type α) (fparam : context α)
 
 structure fdecl {α : Type} [global_names α]
-{self : class_name α} (f : field_name self) : Type 1 :=
+  {self : class_name α} (f : field_name self) : Type 1 :=
 (type : type α)
 
-structure cdecl {α : Type} [global_names α] (self : class_name α) : Type 1 :=
+structure cdecl {α : Type} [global_names α]
+(self : class_name α) : Type 1 :=
 (fdecl (f : field_name self) : fdecl f)
 (mdecl (m : method_name self) : mdecl m)
 
 class signature (α : Type) extends global_names α : Type 1 :=
-(cdef (x : class_name α): cdecl x)
-(main: class_name α)
+(cdef (x : class_name α): cdecl x) (main: class_name α)
 
 def signature.method_params {α : Type} [signature α]
-{self : class_name α} (m : method_name self) : context α :=
-    ((signature.cdef self).mdecl m).fparam
+  {self : class_name α} (m : method_name self) : context α :=
+((signature.cdef self).mdecl m).fparam
 
 def signature.return_type {α : Type} [signature α]
-{self : class_name α} (m : method_name self) : type α :=
-    ((signature.cdef self).mdecl m).retype
+  {self : class_name α} (m : method_name self) : type α :=
+((signature.cdef self).mdecl m).retype
 
 def signature.field_type {α : Type} [signature α]
-{self : class_name α} (f : field_name self) : type α :=
-    ((signature.cdef self).fdecl f).type
+  {self : class_name α} (f : field_name self) : type α :=
+((signature.cdef self).fdecl f).type
 
 open signature
 
-/-
-Given a signature, we consider type environments to consist of:
-a return type,
-a method reference, and
-declared local variables.
--/
+/- Given a signature, we consider type environments to consist of: a return type, a method reference, and declared local variables. -/
 structure tenv (α : Type) [signature α] : Type 1 :=
 (self : class_name α)
 (current : method_name self)
 (locals : context α)
 
-/-
-A note on terminology.
-Parameter: as declared in signature
-Argument: as supplied in a method call
--/
+/- A note on terminology. Parameter: as declared in signature. Argument: as supplied in a method call. -/
 
-/-
-Within a typing environment, we refer to numerous things.
-A parameter refers to the
-index within the parameters of the current method.
-A local refers to the
-index within the declared local variables.
-A field refers to a field within the enclosed class.
-A variable refers to either an argument, a local, or a field.
--/
-
+/- Within a typing environment, we refer to numerous things. A parameter refers to the index within the parameters of the current method. A local refers to the index within the declared local variables. A field refers to a field within the enclosed class. A variable refers to either an argument, a local, or a field. -/
 structure pvar {α : Type} [signature α] (e : tenv α)
-(ty : type α) : Type 1 :=
+  (ty : type α) : Type 1 :=
 (idx : list_at ty (method_params e.current))
 
 structure lvar {α : Type} [signature α] (e : tenv α)
-(ty : type α) : Type 1 :=
-(idx : list_at ty e.locals)
+  (ty : type α) : Type 1
+:= (idx : list_at ty e.locals)
 
 structure fvar {α : Type} [signature α] (e : tenv α)
-(ty : type α) : Type 1 :=
-(idx : field_name e.self)
-(H : ty = field_type idx)
+  (ty : type α) : Type 1
+:= (idx : field_name e.self) (H : ty = field_type idx)
 
 -- Store variable (LHS only)
 inductive svar {α : Type} [signature α] (e : tenv α)
-(ty : type α) : Type 1
+  (ty : type α) : Type 1
 | fvar: fvar e ty → svar
 | lvar: lvar e ty → svar
 
 -- Read variable (RHS only)
 inductive rvar {α : Type} [signature α] (e : tenv α)
-(ty : type α) : Type 1
+  (ty : type α) : Type 1
 | fvar: fvar e ty → rvar
 | pvar: pvar e ty → rvar
 | lvar: lvar e ty → rvar
 
-/-
-Given a typing environment and a list of types,
-we have an argument list of variables with matching types.
--/
+/- Given a typing environment and a list of types, we have an argument list of variables with matching types. -/
 inductive arglist {α : Type} [signature α] (e : tenv α) :
-list (type α) → Type 1
+  list (type α) → Type 1
 | nil : arglist []
-| cons (x : type α) (xs : list (type α)) :
-    rvar e x → arglist xs → arglist (x::xs)
+| cons (x : type α) (xs : list (type α)) : rvar e x → arglist xs → arglist (x::xs)
 
-/-
-A pure expression evaluates to a value of a particular type.
-Such expressions are either:
-a constant data value,
-a function application on data values,
-value lookup in environment,
-referential equality check.
--/
-inductive pexp {α : Type} [signature α] (e : tenv α) : type α → Type 1
+/- A pure expression evaluates to a value of a particular type. Such expressions are either: a constant data value, a function application on data values, value lookup in environment, referential equality check. -/
+inductive pexp {α : Type} [signature α] (e : tenv α) :
+  type α → Type 1
 | value (γ : Type) : γ → pexp γ
 | apply (γ δ : Type) : pexp (γ → δ) → pexp γ → pexp δ
 | lookup {ty : type α} : rvar e ty → pexp ty
 | requal (c : class_name α) : pexp (ref c) → pexp (ref c) → pexp bool
 
-/-
-A statement within a typing environment is either:
-a skip,
-a sequential composition,
-a branch,
-a loop,
-an assignment,
-an asynchronous method call,
-an object allocation.
--/
+/- A statement within a typing environment is either: a skip, a sequential composition, a branch, a loop, an assignment, an asynchronous method call, an object allocation. -/
 inductive stmt {α : Type} [signature α] (e : tenv α) : Type 1
 | skip: stmt
 | seq: stmt → stmt → stmt
@@ -190,23 +130,14 @@ inductive stmt {α : Type} [signature α] (e : tenv α) : Type 1
 | assign {ty : type α} (l : svar e ty): pexp e ty → stmt
 -- call method m, on object in r, with args τ
 | async (c : class_name α) (r : rvar e (ref c))
-    (m : method_name c) (τ : arglist e (method_params m)): stmt
+  (m : method_name c) (τ : arglist e (method_params m)): stmt
 -- allocate new object of class c and store reference in l
 | alloc (c : class_name α) (l : svar e (ref c)): stmt
 
-/-
-A program with a given program signature associates
-to each method of each class a program block.
-
-A program block associated to a method consists of:
-local variable declarations, and
-a statement within a typing environment.
--/
-
+/- A program with a given program signature associates to each method of each class a program block. A program block associated to a method consists of: local variable declarations, and a statement within a typing environment. -/
 structure pblock {α : Type} [signature α]
-{self : class_name α} (m : method_name self) : Type 1 :=
-(locals : context α)
-(S : stmt (tenv.mk self m locals))
+  {self : class_name α} (m : method_name self) : Type 1 :=
+(locals : context α) (S : stmt (tenv.mk self m locals))
 
 structure program {α : Type} [signature α] : Type 1 :=
 (body {self : class_name α} (m : method_name self): pblock m)
